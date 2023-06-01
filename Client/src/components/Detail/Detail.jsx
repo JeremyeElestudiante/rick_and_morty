ac9b34589f03af6b74aa9babb58daa9087589631
Jeremyxa11 <jeremyfernandez178@gmail.com>
2023-06-01 11:54:01 -0500

    Todo funciona bien, falta resolver el bug de cartas repetidas

---
+++ Client/src/components/Detail/Detail.jsx
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react"
+
+export default function Detail(){
+    
+    const {id} = useParams()
+
+    const [character, setCharacter] = useState({})
+
+    useEffect(() => {
+        axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+           if (data.name) {
+              setCharacter(data);
+           } else {
+              window.alert('No hay personajes con ese ID');
+           }
+        });
+        return setCharacter({});
+     }, [id]);
+
+    return(
+        <div style={{backgroundColor: "lightgray"}}>
+            <h1>DETAIL</h1>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={character.name} />
+            <h4>Origin | {character.origin?.name}</h4>
+            <h4>Specie | {character.specie}</h4>
+            <h4>Gender | {character.gender}</h4>
+        </div>
+    )
+}
---
+++ Client/src/redux/reducer.js
@@ -0,0 +1,40 @@
+import {ADD_FAV, FILTER, ORDER, REMOVE_FAV} from "./action-types"
+
+const initialState = {
+    myFavorites: [],
+    allCharacters: []
+}
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case ADD_FAV:
+            return {...state,
+                myFavorites: [...state.allCharacters, action.payload],
+                allCharacters: [...state.allCharacters, action.payload]
+            }
+        case REMOVE_FAV:
+            return {...state, myFavorites: [...state.myFavorites.filter(fav=> fav.id !== parseInt(action.payload))]}
+        case FILTER:
+            const allCharactersFiltered = state.allCharacters.filter(character=> character.gender === action.payload)
+                return {
+                    ...state,
+                    myFavorites: 
+                        action.payload === 'allCharacters'
+                        ? [...state.allCharacters]
+                        : allCharactersFiltered
+                }
+            case ORDER:
+                const allCharactersFavCopy = [...state.allCharacters]
+                return{
+                    ...state,
+                    myFavorites:
+                        action.payload === 'A'
+                        ? allCharactersFavCopy.sort((a,b)=> a.id - b.id)
+                        : allCharactersFavCopy.sort((a,b)=> b.id - a.id)
+                }  
+            default:
+            return {...state}
+    }
+}
+
+export default  reducer
